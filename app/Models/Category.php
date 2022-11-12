9662d30760f06e3eab5dc7eebb44012346ee53f9
Itamar-Ichaves <itmchaves@gmail.com>
2022-11-11 22:12:39 -0300

    terceiro commit

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class category extends Model
+class Category extends Model
 {
-
     use HasFactory;
 
     protected $fillable = ['title', 'url', 'description'];
+
+    public function companies()
+    {
+        return $this->hasMany(Company::class);
+    }
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/users';
+    public const HOME = '/home';
 
     /**
      * The controller namespace for the application.
@@ -39,8 +39,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+                    ->namespace($this->namespace)
+                    ->group(base_path('routes/api.php'));
         });
     }
 
@@ -52,7 +52,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
     }
 }
